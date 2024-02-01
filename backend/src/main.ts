483304c436588692660a705bd6e7b9794a845338
Andriy22 <istep.andriy@gmail.com>
2024-02-01 20:25:13 +0200

    added open api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Crow Messenger API')
+    .setDescription('Crow Messenger OPEN API')
+    .setVersion('1.0')
+    .addTag('Crows')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
