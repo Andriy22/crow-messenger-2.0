aadf9b891645fe17438621b2a59924adfeb4f03f
Andriy22 <istep.andriy@gmail.com>
2024-02-01 21:58:58 +0200

    added db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: `${process.env.DATABASE_HOST}`,
+      port: 1433,
+      username: `${process.env.DATABASE_USER}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`,
+      entities: [],
+      synchronize: true,
+      options: {
+        trustServerCertificate: true,
+      },
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
